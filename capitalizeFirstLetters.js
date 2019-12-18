df29ac106ad91d3dbfb162510a8029ed968792a9
letizia obiglio <letizia.obiglio@gmail.com>
2019-12-18 15:27:40 +0100

    feat: git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,11 +1,25 @@
 const assert = require('assert');
 
-assert.strictEqual(typeof capitalizeFirst, 'function');
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 
-assert.strictEqual(capitalizeFirst.length, 1);
+assert.strictEqual(capitalizeFirstLetters.length, 1);
 
-assert.strictEqual(capitalizeFirst('jje vais en ville'), 'Je Vais En Ville');
+assert.strictEqual(capitalizeFirstLetters('je vais en ville'), 'Je Vais En Ville');
 
-assert.strictEqual(capitalizeFirst('b'), 'B');
+assert.strictEqual(capitalizeFirstLetters('b'), 'B');
 
-assert.strictEqual(capitalizeFirst(''), '');
+assert.strictEqual(capitalizeFirstLetters(''), '');
+
+
+function capitalizeFirstLetters(input) {
+    if (input.length > 0) {
+
+        input = input.split(" ");
+        for (let i = 0; i < input.length; i++) {
+            input[i] = input[i][0].toUpperCase() + input[i].substr(1);
+        }
+        return input.join(' ');
+    } else {
+        return '';
+    }
+}
